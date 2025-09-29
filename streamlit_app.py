95168b2a75323f10b099ffac67c594bfb26595b1
Eduard Barbu <115542962+RKThirteen@users.noreply.github.com>
2025-09-29 14:47:46 +0300

    Update API call to use dynamic ingredient

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,7 +39,7 @@ if ingredients_list:
     for ingredient in ingredients_list:
       ingredients_string+=ingredient+' '
       st.subheader('Nutritional Information for '+ingredient)
-      smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+      smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+ingredient)
       sf_df=st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
     #st.write(ingredients_string)
 
