cf619e3f498db8f1e8a7306702919b57a6357ab3
Eduard Barbu <115542962+RKThirteen@users.noreply.github.com>
2025-09-29 16:00:44 +0300

    Update API call to use search_on variable

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,7 +47,7 @@ if ingredients_list:
       st.write('The search value for ', ingredient,' is ', search_on, '.')
       
       st.subheader('Nutritional Information for '+ingredient)
-      smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+ingredient)
+      smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+search_on)
       sf_df=st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
     #st.write(ingredients_string)
 
