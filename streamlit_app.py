cef6250565098e22ac9575b7a88a0179217b2926
Eduard Barbu <115542962+RKThirteen@users.noreply.github.com>
2025-09-29 12:48:09 +0300

    Parse JSON response for smoothie order

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,7 +46,7 @@ if ingredients_list:
     #st.write(my_insert_stmt)
     #st.stop()
     smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-    st.text(smoothiefroot_response)
+    st.text(smoothiefroot_response.json())
     time_to_insert=st.button('Submit Order')
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
