4edc02c1e495dda812e1790ec54a2f2cceac17ae
Eduard Barbu <115542962+RKThirteen@users.noreply.github.com>
2025-09-29 15:58:14 +0300

    Comment out dataframe display and stop
    Comment out dataframe display and stop execution.

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,8 +26,8 @@ cnx=st.connection("snowflake")
 session=cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
 pd_df=my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
+#st.dataframe(pd_df)
+#st.stop()
 
 
 ingredients_list=st.multiselect(
@@ -44,8 +44,8 @@ if ingredients_list:
       ingredients_string+=ingredient+' '
       
       search_on=pd_df.loc[pd_df['FRUIT_NAME'] == ingredient, 'SEARCH_ON'].iloc[0]
-      
       st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+      
       st.subheader('Nutritional Information for '+ingredient)
       smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+ingredient)
       sf_df=st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
